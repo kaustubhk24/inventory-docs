2dff850f2e6ea95e9dc2391d0a5286ed48e7673b
Kaustubh Kulkarni <57136442+kaustubhk24@users.noreply.github.com>
2021-07-21 15:02:38 +0530

    Updates

---
+++ assets/js/814f3328.cedc1e35.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinventory_docs=self.webpackChunkinventory_docs||[]).push([[535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
